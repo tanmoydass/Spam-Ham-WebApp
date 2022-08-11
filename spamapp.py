426663aa34a2ae5714ea3c4af33e4ae3dfc3aef2
Tanmoy Das <110119082+tanmoydass@users.noreply.github.com>
2022-08-11 23:39:26 +0530

    Update spamapp.py

--- spamapp.py
+++ spamapp.py
@@ -25,9 +25,4 @@ ip = st.text_input('Enter the message') #creates a text box in web app
 op = model_nb.predict([ip])
 if st.button('Predict'): # st.button will create a button with name Predict
   st.title(op[0]) # the output will be displayed as a title
-  st.balloons()
-
-    
-    
-    
-  
+  st.balloons()  
